3d6792383a3a458f208e389b20c539f2abf37d7f
dyehorov <114303640+dyehorov@users.noreply.github.com>
2025-12-19 13:50:00 +0100

    feat: enhance movie catalog UI with search functionality, filter options, and modal for full movie descriptions

--- js/codewars/script.js
+++ js/codewars/script.js
@@ -145,6 +145,9 @@ console.log(rowSumOddNumbers(42))
 
 /*
  1 3 7 13 21 each step is multiplied by 2, then we can say an​=n2−n+1 then 
+*/
 
-
- */
+/*
+ The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}. 
+*/
--- js/practice/practice-10-movie-catalog/scripts/script.js
+++ js/practice/practice-10-movie-catalog/scripts/script.js
@@ -1,1 +1,14 @@
 const KEY = "123116da"
+const openFullDescriptionButton = document.querySelector(".movie-button")
+const movieFullDescriptionContainer = document.querySelector(
+  ".movie-full-description"
+)
+const closeFullDescriptionModal = document.querySelector(".close-modal-button")
+
+openFullDescriptionButton.addEventListener("click", () => {
+  movieFullDescriptionContainer.classList.remove("modal-hidden")
+})
+
+closeFullDescriptionModal.addEventListener("click", () => {
+  movieFullDescriptionContainer.classList.add("modal-hidden")
+})
