aa9340dc31d1a106fad2ded7db2ae5746b0a4b48
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-01-27 12:31:12 +0100

    Initialize React project with Vite setup, including basic components and configuration files

---
+++ react/classworks/lesson-1-intro/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./components/test"
+
+function App() {
+  return (
+    <div>
+      <h1>React project</h1>
+      <Test />
+    </div>
+  )
+}
+
+export default App
---
+++ react/classworks/lesson-1-intro/src/components/test/index.jsx
@@ -0,0 +1,5 @@
+function Test(){
+    return <p>Lorem ipsum dolor sit amet.</p>
+}
+
+export default Test
