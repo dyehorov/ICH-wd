bfb5d9f8011c74cd42cdee2aecad0a1558b3d1d6
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-02-09 11:42:47 +0100

    feat: sponsors section done

--- react/projects/cooljob/src/App.jsx
+++ react/projects/cooljob/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import Header from "./components/header"
 import Hero from "./components/hero"
+import TrustedBy from "./components/trustedBy"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container">
         <Header />
         <Hero />
+        <TrustedBy />
       </div>
     </>
   )
