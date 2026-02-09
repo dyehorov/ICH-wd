ecf14e5503a367e95b766862a3957c4d643e7e53
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-02-09 09:27:02 +0100

    init cooljob project

---
+++ react/projects/cooljob/src/components/logo/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css"
+import logo from "../../assets/cooljob.svg"
+
+export default function Logo() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  )
+}
