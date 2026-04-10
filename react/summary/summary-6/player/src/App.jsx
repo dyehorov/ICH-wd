834b4311d7c043a98979505ce76b7db49bc2bd5c
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-04-10 11:26:50 +0200

    feat: summary-6-player: implement player controls and features

--- react/summary/summary-6/player/src/App.jsx
+++ react/summary/summary-6/player/src/App.jsx
@@ -1,10 +1,97 @@
 import "./App.css"
+import { useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import {
+  playPause,
+  setTime,
+  changeVolume,
+  toggleMute,
+  nextRepeatMode,
+  setPlaybackRate,
+  seekForward,
+  seekBackward,
+} from "./redux/slices/playerSlice"
 
 export default function App() {
   const dispatch = useDispatch()
 
-  const playerVolume = useSelector(state => state.player.volume)
+  const [seekValue, setSeekValue] = useState("")
 
-  return <h1>{playerVolume}</h1>
+  const {
+    volume,
+    isPlaying,
+    currentTime,
+    maxTime,
+    isMuted,
+    previousVolume,
+    repeatMode,
+    playbackRate,
+  } = useSelector(state => state.player)
+
+  return (
+    <>
+      <h1>{isPlaying ? "Playing" : "On pause"}</h1>
+      <button onClick={() => dispatch(playPause())}>Play/Pause</button>
+
+      <div style={{ marginTop: 16 }}>
+        <div>{`${currentTime} / ${maxTime}`}</div>
+
+        <input
+          type="number"
+          placeholder="Enter time"
+          onChange={e => dispatch(setTime(Number(e.target.value)))}
+        />
+      </div>
+      <div style={{ marginTop: 16 }}>
+        <p>Volume: {volume}</p>
+        <p>{isMuted ? "muted" : "not muted"}</p>
+        <p>Previous Volume: {previousVolume}</p>
+
+        <input
+          type="number"
+          placeholder="Enter volume"
+          onChange={e => dispatch(changeVolume(Number(e.target.value)))}
+        />
+        <button onClick={() => dispatch(toggleMute())}>
+          {isMuted ? "Unmute" : "Mute"}
+        </button>
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <button onClick={() => dispatch(nextRepeatMode())}>
+          Repeat: {repeatMode}
+        </button>
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <p>Playback Rate: {playbackRate}</p>
+
+        <input
+          type="number"
+          placeholder="Enter volume"
+          onChange={e => dispatch(setPlaybackRate(Number(e.target.value)))}
+        />
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <p>Current time: {currentTime}</p>
+
+        <input
+          type="number"
+          placeholder="Seconds"
+          value={seekValue}
+          onChange={event => {
+            setSeekValue(Number(event.target.value))
+          }}
+        />
+
+        <button onClick={() => dispatch(seekBackward(seekValue))}>
+          Backward
+        </button>
+        <button onClick={() => dispatch(seekForward(seekValue))}>
+          Forward
+        </button>
+      </div>
+    </>
+  )
 }
--- react/summary/summary-6/player/src/redux/slices/playerSlice.js
+++ react/summary/summary-6/player/src/redux/slices/playerSlice.js
@@ -15,13 +15,98 @@ const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    reset: (state, action) => {
-      state.data = null
-      localStorage.setItem("auth", null)
+    playPause: state => {
+      state.isPlaying = !state.isPlaying
+    },
+    setTime: (state, action) => {
+      if (action.payload < 0) {
+        state.currentTime = 0
+      } else if (action.payload > state.maxTime) {
+        state.currentTime = state.maxTime
+      } else {
+        state.currentTime = action.payload
+      }
+    },
+    changeVolume: (state, action) => {
+      const newVolume = action.payload
+
+      if (newVolume > 0) {
+        state.previousVolume = newVolume
+      }
+
+      if (newVolume > 100) {
+        state.volume = 100
+      } else {
+        state.volume = newVolume
+      }
+
+      if (newVolume === 0) {
+        state.isMuted = true
+      } else {
+        state.isMuted = false
+      }
+    },
+    toggleMute: state => {
+      if (!state.isMuted) {
+        if (state.volume > 0) {
+          state.previousVolume = state.volume
+        }
+
+        state.volume = 0
+        state.isMuted = true
+      } else {
+        state.volume = state.previousVolume
+        state.isMuted = false
+      }
+    },
+    nextRepeatMode: state => {
+      const modes = ["none", "one", "all"]
+      const currentIndex = modes.indexOf(state.repeatMode)
+
+      if (currentIndex === -1) currentIndex = 0
+
+      let nextIndex
+
+      if (currentIndex === modes.length - 1) {
+        nextIndex = 0
+      } else {
+        nextIndex = currentIndex + 1
+      }
+
+      state.repeatMode = modes[nextIndex]
+    },
+    setPlaybackRate: (state, action) => {
+      const allowedRates = [0.5, 0.75, 1.0, 1.25, 1.5]
+
+      const isValueAllowed = allowedRates.find(item => item === action.payload)
+
+      if (isValueAllowed) {
+        state.playbackRate = action.payload
+      }
+    },
+    seekForward: (state, action) => {
+      const seconds = action.payload
+
+      state.currentTime = Math.min(state.currentTime + seconds, state.maxTime)
+    },
+
+    seekBackward: (state, action) => {
+      const seconds = action.payload
+
+      state.currentTime = Math.max(state.currentTime - seconds, 0)
     },
   },
 })
 
-export const { reset } = playerSlice.actions
+export const {
+  playPause,
+  setTime,
+  changeVolume,
+  toggleMute,
+  nextRepeatMode,
+  setPlaybackRate,
+  seekForward,
+  seekBackward,
+} = playerSlice.actions
 
 export default playerSlice.reducer
