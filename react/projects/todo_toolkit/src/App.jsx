cad9e8141b5e10e9528c0d5a8ec944311118d72c
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-04-13 11:36:44 +0200

    feat: added to chrome extensions

--- react/projects/todo_toolkit/src/App.jsx
+++ react/projects/todo_toolkit/src/App.jsx
@@ -4,9 +4,9 @@ import Header from "./components/header"
 
 export default function App() {
   return (
-    <>
+    <div style={{ minWidth: "540px", maxWidth: "100%" }}>
       <Header />
       <Main />
-    </>
+    </div>
   )
 }
